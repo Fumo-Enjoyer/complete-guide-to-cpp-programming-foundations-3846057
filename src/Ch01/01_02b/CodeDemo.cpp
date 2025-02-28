e67237e2e68ca604de2d6d92bc98f74f33d5581b
Fumo-Enjoyer <a-mcdaniel@outlook.com>
2025-02-28 21:09:34 +0000

    Followed from the video

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,12 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+
+int main(){
+    std::cout << "Hi There!" << std::endl;
+
+    std::cout << std::endl << std::endl; //This adds 2 spaces after the code finishes to space out the results and debugger jargon
+    return 0;
+}
